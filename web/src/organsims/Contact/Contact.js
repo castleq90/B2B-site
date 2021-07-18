7446a0d6652a40fbd621c893b872106076d10a50
오선주 <oceanzoo@oseonjuui-MacBookPro.local>
2021-07-18 12:43:54 +0900

    contact 레이아웃 완료

--- web/src/organsims/Contact/Contact.js
+++ web/src/organsims/Contact/Contact.js
@@ -1,10 +1,54 @@
 import React from 'react'
+import './Contact.scss'
+
+const CONTACT = [
+  {
+    name: '오선주',
+    number: '01011113333'
+  },
+  {
+    name: '이정민',
+    number: '01022223333'
+  },
+  {
+    name: '김성규',
+    number: '01044443333'
+  },
+  {
+    name: '강준희',
+    number: '01012345670'
+  },
+  {
+    name: '김건우',
+    number: '01022345566'
+  },
+  {
+    name:'김지민',
+    number:'01022223342'
+  },
+  {
+    name:'이도윤',
+    number:'01012340987'
+  },
+  {
+    name:'한성봉',
+    number:'01098765432',
+  },
+  {
+    name:'배찬영',
+    number:'01046533243',
+  },
+  {
+    name:'최승리',
+    number:'01009877777'
+  }
+]
 
 export default function Contact() {
   return (
     <div className="contactContainer">
       <div className="contactHeader">
-        <span className="contactText">연락처</span>
+        <span>연락처</span>
         <i className="fas fa-plus"/>
       </div>
       <div className="contactSearch">
@@ -14,9 +58,18 @@ export default function Contact() {
           </span>
       </div>
       <div className="contactInfo">
-        <div className="name">
-          이름
-        </div>
+        {CONTACT.map((person, index)=>{
+          return(
+            <div className="infoWrapper">
+              <p key={index} className="name">
+                {person.name}
+              </p>
+              <p className="phoneNumber">
+                {person.number}
+              </p>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
