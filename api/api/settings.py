4eb43c774d7dd57d46342453a3a98bf8b155cd51
E5presso <sejong418@icloud.com>
2021-07-21 18:57:25 +0900

    initialize database and resolve conflictions

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,8 +28,12 @@ SECRET_KEY = 'bdf)aw3l3rxf-$#$b3^fbzly(^=s^nbu&f@1zmc)x^okv!yahf'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wecode',
+        'USER': 'wecode21',
+        'PASSWORD': 'wecode21',
+        'HOST': 'wecode-intern-21.cx1nsexwbsnv.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
