12367534d8663133d75fa6e309c7056054582c50
김성규 <castleq37@gmail.com>
2021-07-26 10:58:16 +0900

    ADD: 로그인 기능 구현

--- api/users/views/user.py
+++ api/users/views/user.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 from knox.models import AuthToken
 from knox.views import LoginView as KnoxLoginView
@@ -15,18 +16,24 @@ class CreateUserGenericView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
-        "user": UserCreateSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
-        })
+                "user": UserCreateSerializer(user, context=self.get_serializer_context()).data},
+            headers={
+                "Authorization": AuthToken.objects.create(user)
+                },
+                status=status.HTTP_200_OK)
 
 class SigninAPI(generics.GenericAPIView):
     serializer_class = UserSigninSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user),
-        })
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data
+            return Response({
+                "user": UserSerializer(user, context=self.get_serializer_context()).data},
+            headers={
+                "Authorization": AuthToken.objects.create(user)
+                },
+                status=status.HTTP_200_OK)
+        else:
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
